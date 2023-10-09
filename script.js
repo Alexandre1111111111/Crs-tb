a7e62d0c0193a18ecb77b3e6f79e3b54554e0ed1
Alexandre1111111111 <127530540+Alexandre1111111111@users.noreply.github.com>
2023-10-09 09:02:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,22 +3,6 @@ const cons = document.querySelector("#cons");
 const ongs = document.querySelector(".ongs");
 const btn_ong = document.querySelector("#btn_ong");
 
-mpd.addEventListener("click", () => {
-    if(mpd.style.height == "300px") {
-        mpd.style.height = "700px";
-        mpd.style.top = "0px";
-        mpd.style.right = "0px";
-        cons.style.display = "none";
-        mpd.title="Clique para Retrair"
-    }
-    else {
-        mpd.style.height = "300px";
-        cons.style.display = "block";
-        mpd.style.top = "190px";
-        mpd.style.right = "30px";
-        mpd.title="Clique para Expandir"
-    }
-})
 mpd.addEventListener("mouseover", () => {
     mpd.style.border = "3px solid #20973a"; 
 })
